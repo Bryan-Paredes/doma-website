7e603bbce7146789214c1aeec724b36fd8e7d5f4
Bryan Paredes <bryansparedes2@gmail.com>
2024-11-13 21:14:01 -0600

    astro config and package installation

---
+++ src/components/social-item.js
@@ -0,0 +1,17 @@
+export const socialItems = [
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/mailkit/",
+    icon: "/icons/instagram.svg",
+  },
+  {
+    name: "Facebook",
+    href: "https://www.facebook.com/mailkit/",
+    icon: "/icons/facebook.svg",
+  },
+  {
+    name: "XSocial",
+    href: "https://twitter.com/mailkit/",
+    icon: "/icons/X.svg",
+  },
+];
